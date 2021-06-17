7af791c9be13c65487f1b068d1cad15a54ecc318
Đỗ Việt Hoàng <hoangdv2429@gmail.com>
2021-06-17 23:53:27 +0700

    some features need to be improve

---
+++ gk/incompleted/mytest.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+./kplc ../test/example1.kpl | diff ../test/result1.txt -
+./kplc ../test/example2.kpl | diff ../test/result2.txt -
+./kplc ../test/example3.kpl | diff ../test/result3.txt -
+./kplc ../test/example4.kpl | diff ../test/result4.txt -
+./kplc ../test/example5.kpl | diff ../test/result5.txt -
+./kplc ../test/example6.kpl | diff ../test/result6.txt -
+./kplc ../test/example7.kpl | diff ../test/result7.txt -
